a550f9ec4d9608adf2d534f0aa9564e18471b120
jim.mruzik <jim.mruzik@asynchrony.com>
2015-01-23 23:41:41 -0600

    Adding jquery, pulling text for task from javascript function

---
+++ www/js/task.js
@@ -0,0 +1,8 @@
+var task = {
+    // Application Constructor
+    initialize: function() {
+		$("div#task_field").text("Find and touch a nearby brick with one finger, is anyone else doing the same?");
+	}
+};
+
+task.initialize();
