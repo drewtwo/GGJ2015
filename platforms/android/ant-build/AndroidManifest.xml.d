ba19f3cafe1e573f6aa7c2b7f230a965c935e210
jim.mruzik <jim.mruzik@asynchrony.com>
2015-01-23 22:31:57 -0600

    001 - First commit of hello world cordova project - Jim

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/jim.mruzik/gamejam2015/hello/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/jim.mruzik/gamejam2015/hello/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/Users/jim.mruzik/gamejam2015/hello/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
+/Users/jim.mruzik/gamejam2015/hello/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/jim.mruzik/gamejam2015/hello/platforms/android/ant-build/classes.dex \
+/Users/jim.mruzik/gamejam2015/hello/platforms/android/CordovaLib/ant-build/classes.jar \
