c07da4cb09bf45255a4d53309ccd1111cdbcb6d7
jim.mruzik <jim.mruzik@asynchrony.com>
2015-01-24 10:11:20 -0600

    Added tasks.json, and parsing of that data

--- www/js/task.js
+++ www/js/task.js
@@ -1,9 +1,9 @@
 var task = {
     // Application Constructor
     initialize: function() {
-		var possibleTasks = ["Find and touch a nearby brick with one finger, is anyone else doing the same?"
-		,"Touch your nose with two fingers, is anyone else doing the same?"];
-		$("div#task_field").text(possibleTasks[this.getRandomInt(0,1)]);
+		$.getJSON("json/tasks.json", function(taskJson) {
+			$("div#task_field").text(taskJson[0].messages[task.getRandomInt(0,1)]);
+		});
 	},
 	getRandomInt: function(min, max) {
 	  return Math.floor(Math.random() * (max - min + 1)) + min;
