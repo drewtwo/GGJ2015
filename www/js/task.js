bbceee3a031bd797e86a86b8737b8ede292eccea
jim.mruzik <jim.mruzik@asynchrony.com>
2015-01-24 00:01:45 -0600

    random message on task page

--- www/js/task.js
+++ www/js/task.js
@@ -1,7 +1,12 @@
 var task = {
     // Application Constructor
     initialize: function() {
-		$("div#task_field").text("Find and touch a nearby brick with one finger, is anyone else doing the same?");
+		var possibleTasks = ["Find and touch a nearby brick with one finger, is anyone else doing the same?"
+		,"Touch your nose with two fingers, is anyone else doing the same?"];
+		$("div#task_field").text(possibleTasks[this.getRandomInt(0,1)]);
+	},
+	getRandomInt: function(min, max) {
+	  return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 };
 
