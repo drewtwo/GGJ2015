28e7cc0a8499d12d0492b545ea5c1e14323090de
jim.mruzik <jim.mruzik@asynchrony.com>
2015-01-25 13:42:24 -0600

    removed image

--- www/js/viewInteractions.js
+++ www/js/viewInteractions.js
@@ -4,9 +4,7 @@ var viewInteractions = {
 		interactions.forEach(function(interaction) { 
 			var date = $("<p></p>").text(JSON.stringify(interaction["date"]));
 			var interaction = $("<p></p>").text(JSON.stringify(interaction["interaction"]));
-			var image = $("<img></img>");
-			image.attr("src", "data:image/jpeg;base64,"+JSON.stringify(interaction["imageURI"]));
-			$("div#interactions").after(date, interaction, image);
+			$("div#interactions").after(date, interaction);
 		});
 	},
 	backToMainScreen: function() {
